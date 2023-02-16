99f3914f5c49be1c854fd7017367858de5c02d0d
SandhyaSravana221 <sandhyasravana221@gmail.com>
2023-02-16 11:39:53 +0530

    initial

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ app.get("/", (_req, res) => {
     );
 });
 
-// app.post("/users", (req, res) => {
-//   console.log(req.body);
-//   res.status(201).send({
-//     id: Math.random() * 1e5,
-//     name: req.body.name,
-//     email: req.body.email,
-//     firstName: req.body.firstName,
-//     lastName: req.body.lastName,
-//     isActive: true,
-//     role: "student",
-//   });
-// });
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  res.status(201).send({
+    id: Math.random() * 1e5,
+    name: req.body.name,
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    isActive: true,
+    role: "student",
+  });
+});
 
 module.exports = app;
 // Path: app.js
